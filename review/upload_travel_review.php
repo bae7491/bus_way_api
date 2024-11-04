778885ec54216e3fa5b02a57aadc7010718f21aa
bae7491 <bae7491@naver.com>
2024-11-04 11:03:38 +0900

    관광지 리뷰 작성 이미지가 없을 때, null로 추가하도록 변경

--- review/upload_travel_review.php
+++ review/upload_travel_review.php
@@ -3,18 +3,25 @@
 
 include "../connection.php";
 
-$image = $_FILES['image']['name'];
-$imagePath = "../images/uploads/{$image}";
-$tmp_name = $_FILES['image']['tmp_name'];
-
-move_uploaded_file($tmp_name, $imagePath);
-
 $email = $_POST['email'];
 $review_rate = $_POST['review_rate'];
 $review_content = $_POST['review_content'];
 
-// SQL 쿼리 작성
-$sqlQuery = "INSERT INTO travel_review (email, review_rate, review_content, review_image) VALUES ('$email', '$review_rate', '$review_content', '$image')";
+// 이미지가 있는지 확인 (없으면 null로 설정)
+if (isset($_FILES['image']) && $_FILES['image']['error'] == UPLOAD_ERR_OK) {
+    $image = $_FILES['image']['name'];
+    $imagePath = "../images/uploads/{$image}";
+    $tmp_name = $_FILES['image']['tmp_name'];
+
+    move_uploaded_file($tmp_name, $imagePath) 
+    ? $image_path = $image 
+    : $image_path = null;
+} else {
+    $image_path = null;
+}
+
+// SQL 쿼리 작성: 이미지가 있는 경우와 없는 경우에 따라 쿼리를 달리 작성
+$sqlQuery = "INSERT INTO travel_review (email, review_rate, review_content, review_image) VALUES ('$email', '$review_rate', '$review_content', " . ($image_path ? "'$image_path'" : "NULL") . ")";
 
 if ($con->query($sqlQuery) === TRUE) {
     echo json_encode([
