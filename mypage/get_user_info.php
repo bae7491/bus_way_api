406e3de0b7c19f9b54d33f198b4fec8d03fad4a5
bae7491 <bae7491@naver.com>
2024-11-07 15:25:02 +0900

    로그인 회원의 정보 조회 API 추가

---
+++ mypage/get_user_info.php
@@ -0,0 +1,25 @@
+<?php
+// 로그인 한 회원 정보를 DB에서 불러오기
+
+include "../connection.php";
+
+$email = $_POST['email'];
+
+// 이메일(고유값)을 이용하여 회원 정보 가져오기
+$sqlQuery = "SELECT * FROM user WHERE email = '$email'";
+
+$result = $con->query($sqlQuery);
+
+if ($result) {
+  $row = $result->fetch_assoc();
+  echo json_encode([
+    "success" => true,
+    "email" => $row['email'],
+    "nickName" => $row['nickName'],
+  ]);
+} else {
+  echo json_encode([
+        "success" => false,
+        "message" => "데이터를 불러오지 못했습니다: {$con->error}",
+    ]);
+}
